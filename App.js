99616149c433a9ec04312954f9381c430c6d8ddf
avisek123 <sahooavisek41@gmail.com>
2022-11-18 23:42:43 +0530

    react-native-stripe package install

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {StripeProvider} from '@stripe/stripe-react-native';
+import PaymentScreen from './Payment';
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
         merchantIdentifier="merchant.identifier" // required for Apple Pay
         urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
       >
-        {/* <PaymentScreen /> */}
+        <PaymentScreen />
       </StripeProvider>
     </View>
   );
