17bcb59792b9e3620199b235cc5ea7b9597f2d10
avisek123 <sahooavisek41@gmail.com>
2022-11-19 00:16:39 +0530

    craete token done

--- Payment.js
+++ Payment.js
@@ -1,29 +1,53 @@
-import {CardField, useStripe} from '@stripe/stripe-react-native';
+import {CardField, createToken, useStripe} from '@stripe/stripe-react-native';
+import {useState} from 'react';
+import {Button} from 'react-native';
 
 export default function PaymentScreen() {
   const {confirmPayment} = useStripe();
+  const [cardInfo, setCardInfo] = useState({});
+  const fetchCardInfo = cardDetails => {
+    setCardInfo(cardDetails);
+  };
+  const onSubmit = async () => {
+    console.log({cardInfo});
+    if (cardInfo) {
+      const resToken = await createToken({
+        ...cardInfo,
+        type: 'Card',
+      });
+
+      console.log({resToken});
+    }
+  };
 
   return (
-    <CardField
-      postalCodeEnabled={false}
-      placeholders={{
-        number: '4242 4242 4242 4242',
-      }}
-      cardStyle={{
-        backgroundColor: '#FFFFFF',
-        textColor: '#000000',
-      }}
-      style={{
-        width: '100%',
-        height: 50,
-        marginVertical: 30,
-      }}
-      onCardChange={cardDetails => {
-        console.log('cardDetails', cardDetails);
-      }}
-      onFocus={focusedField => {
-        console.log('focusField', focusedField);
-      }}
-    />
+    <>
+      <CardField
+        postalCodeEnabled={false}
+        placeholders={{
+          number: '4242 4242 4242 4242',
+        }}
+        cardStyle={{
+          backgroundColor: '#FFFFFF',
+          textColor: '#000000',
+        }}
+        style={{
+          width: '100%',
+          height: 50,
+          marginVertical: 30,
+        }}
+        onCardChange={cardDetails => {
+          fetchCardInfo(cardDetails);
+          console.log('cardDetails', cardDetails);
+        }}
+        onFocus={focusedField => {
+          console.log('focusField', focusedField);
+        }}
+      />
+      <Button
+        color={cardInfo?.complete ? 'green' : 'gray'}
+        onPress={onSubmit}
+        title="Pay"></Button>
+    </>
   );
 }
